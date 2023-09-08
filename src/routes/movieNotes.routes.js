e18f97c6ae520534c2fd136c1906a96491558ba2
Douglas <portogdouglas@gmail.com>
2023-09-08 10:36:39 -0300

    logica do INDEX do movieNote

--- src/routes/movieNotes.routes.js
+++ src/routes/movieNotes.routes.js
@@ -4,8 +4,9 @@ import { movieNotesControllers } from "../Controllers/MovieNotesControllers.js";
 
 export const movieNotesRouter = Router()
 
+movieNotesRouter.get('/:id',movieNotesControllers.show)
 movieNotesRouter.post('/:user_id',movieNotesControllers.create)
 // movieNotesRouter.put('/:user_id',movieNotesControllers.update)
 movieNotesRouter.delete('/:id',movieNotesControllers.delete)
-movieNotesRouter.get('/:id',movieNotesControllers.show)
+movieNotesRouter.get('/',movieNotesControllers.index)
 
