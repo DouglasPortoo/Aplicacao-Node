f2fa2defeaf5d3587125182979fe7980c1d0e99b
Douglas <portogdouglas@gmail.com>
2023-09-08 09:29:43 -0300

    logica do CREATE do movieNotes

--- src/Controllers/MovieNotesControllers.js
+++ src/Controllers/MovieNotesControllers.js
@@ -2,7 +2,29 @@ import { connection } from "../database/knex/index.js";
 
 export const movieNotesControllers = {
   create: async (req, res) => {
+    const { title, description, rating, tags } = req.body
+    const { user_id } = req.params
 
+    const [note_id] = await connection("movie_notes").insert({
+      title,
+      description,
+      rating,
+      user_id
+    })
+
+    const insertTags = tags.map((name)=>{
+      return{
+        note_id,
+        name,
+        user_id
+      }
+    })
+
+    await connection("movie_tags").insert(insertTags)
+
+    res.json("Filme cadastrado")
+
+    
   },
   
   show: async (req, res) => { },
--- src/routes/movieNotes.routes.js
+++ src/routes/movieNotes.routes.js
@@ -4,7 +4,7 @@ import { movieNotesControllers } from "../Controllers/MovieNotesControllers.js";
 
 export const movieNotesRouter = Router()
 
-// movieNotesRouter.post('/',movieNotesControllers.create)
+movieNotesRouter.post('/:user_id',movieNotesControllers.create)
 // movieNotesRouter.put('/:user_id',movieNotesControllers.update)
 // movieNotesRouter.delete('/:id',movieNotesControllers.delete)
 // movieNotesRouter.get('/:user_id',movieNotesControllers.show)
